a932aec13c310f2521e125ebb8dbd7a57699409f
Somogyi Botond <somogyi.botond.munka@gmail.com>
2022-11-08 22:07:58 +0100

    Kesz

--- src/main/java/hu/petrik/calculator/calculator/CalculatorController.java
+++ src/main/java/hu/petrik/calculator/calculator/CalculatorController.java
@@ -1,14 +1,84 @@
 package hu.petrik.calculator.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
-public class HelloController {
+public class CalculatorController {
+
+
+    private double resultFirst;
+    private double resultSec;
     @FXML
-    private Label welcomeText;
+    private Label resultField;
+    @FXML
+    private Spinner<Double> inputFirst;
+    @FXML
+    private Spinner<Double> inputSec;
+    @FXML
+    private Button plusBtn;
+    @FXML
+    private Button minusBtn;
+    @FXML
+    private Button multipleBtn;
+    @FXML
+    private Button divisionBtn;
+    @FXML
+    private Button remDivisionBtn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void clickEvent(ActionEvent actionEvent) {
+        try {
+            resultFirst = inputFirst.getValue();
+            resultSec = inputSec.getValue();
+        }catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Hiba");
+            alert.setContentText("Hibás adatot adott meg!");
+            alert.show();
+            return;
+        }
+        Button btn = (Button) actionEvent.getSource();
+        switch (btn.getId()) {
+            case "plusBtn":
+                plus();
+                break;
+            case "minusBtn":
+                minus();
+                break;
+            case "multipleBtn":
+                multiple();
+                break;
+            case "divisionBtn":
+                division();
+                break;
+            case "remDivisionBtn":
+                remDivision();
+                break;
+        }
+
     }
+
+    private void plus() {
+        setResult(resultFirst + resultSec);
+    }
+    private void minus() {
+        setResult(resultFirst - resultSec);
+
+    }
+    private void multiple() {
+        setResult(resultFirst * resultSec);
+    }
+    private void division() {
+        setResult(resultFirst / resultSec);
+
+    }
+    private void remDivision() {
+        setResult(resultFirst % resultSec);
+    }
+
+    private void setResult(double result) {
+        resultField.setText(String.valueOf(Math.floor(result * 100) / 100));
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hu.petrik.calculator.calculator {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens hu.petrik.calculator.calculator to javafx.fxml;
     exports hu.petrik.calculator.calculator;
 }
